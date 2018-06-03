8213518cd511d37c75ee2541ad3e2d2b25d1b7a7
Matias Nievas Soto <matytaa@gmail.com>
2018-06-03 19:50:37 -0300

    implementación de parseador de contactos, rafactorizar

--- spec/parseador_contacto_spec.rb
+++ spec/parseador_contacto_spec.rb
@@ -1,21 +1,31 @@
 require_relative '../model/parseador_contacto'
+require 'yaml'
 
 class ParseadorContactoSpec
 
+
     describe 'ParseadorContacto' do
-    	
+        
         mi_parseador = ParseadorContacto.new()
-        it 'deberia retornar el nombre' do
-            un_nombre = "kimi"
-            un_apellido = "raikkonen"
-            un_mail = "kimi.raikkonen@unmail.com"
-            una_linea = un_nombre + ","+ un_apellido +"," + un_mail
-        	expect(mi_parseador.parsear(una_linea)).to include(un_nombre)
+        un_archivo = "json_contactos_datos.txt"
+        def levantar_archivo_json(un_archivo, un_id)
+            mi_hash = YAML.load_file(un_archivo)
         end
 
         it 'deberia importar todos los contactos de la lista' do
-            un_archivo = "lista_de_contactos.txt"
-            expect(mi_parseador.levantar_lista_de_contactos(un_archivo)).to eq 5
+            expect(mi_parseador.parsear(levantar_archivo_json(un_archivo, "contactos"), "contactos")).to eq 5
+        end
+
+        it 'deberia poder generar un contacto' do
+            un_json = {"contactos":{"nombre": "kimi","apellido": "raikkonen","mail": "kimi.raikkonen@unmail.com"}}
+            un_contacto = JSON.parse(un_json.to_json)
+            expect(mi_parseador.genera_contactos(un_contacto)).to eq 1
+        end
+
+        it 'deberia tener un array de 5 contactos' do
+            un_parseador = ParseadorContacto.new()
+            un_parseador.parsear(levantar_archivo_json(un_archivo, "contactos"), "contactos")
+            expect(un_parseador.get_contactos().size).to eq 5
         end
     end
 end
