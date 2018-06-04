7757b6c5b8f4fd6b0c3bc9c10ee79e8bf39a8362
Victoria <MVictoriaV>
2018-06-03 23:16:56 -0300

    envio mail

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'json'
 require_relative './model/parseador_json'
+require_relative './model/enviador_de_mail'
 
 @nombre = ''
 
@@ -13,7 +14,17 @@ get '/procesadorDeMail/:nombre' do
 end
 
 post '/' do
-  parseador_json = ParseadorJson.new
-  parseador_json.parsear(request.body.read)
+  #parseador_json = ParseadorJson.new
+  #parseador_json.parsear(request.body.read)
+  envia_mail("mensaje")
   status 200
-end 
+end
+
+def envia_mail(cuerpo)
+  enviador = EnviadorDeMail.new
+  enviador.configurar_mail()
+  enviador.inyectar_cuerpo_del_mail(cuerpo)
+  enviador.inyectar_mail_detino("destino@ejemplo.com")
+  enviador.inyectar_asunto("un asuynto")
+  enviador.enviar_mail()
+end
