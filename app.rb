a75b55fc335378a653cc94556d07e90328631dd9
Victoria <MVictoriaV>
2018-06-04 23:57:25 -0300

    Envio de mail con template

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 require 'json'
+require 'erb'
 require_relative './model/parseador_json'
 require_relative './model/enviador_de_mail'
+require_relative './model/plantilla'
 
 @nombre = ''
 @cuerpo_del_mail = ''
-
-#curl -X POST -H "Content-Type: application/json" --data @data1.json localhost:4567/
 
 get '/procesadorDeMail/:nombre' do
   @nombre = params[:nombre]
@@ -36,11 +36,30 @@ end
 def envia_mail(datos_del_mail, template)
   enviador = EnviadorDeMail.new
   enviador.configurar_mail()
-  enviador.inyectar_cuerpo_del_mail(template)
   enviador.inyectar_asunto(datos_del_mail.asunto)
   enviador.inyectar_mail_origen(datos_del_mail.mail_remitente)
+
   @contactos.each do |contacto|
     enviador.inyectar_mail_detino(contacto.get_mail())
+    cuerpo = agregar_cuerpo(contacto.get_nombre(), datos_del_mail, template)
+    enviador.inyectar_cuerpo_del_mail(cuerpo)
     enviador.enviar_mail()
   end
+end
+
+def agregar_cuerpo(nombre_contacto, datos_del_mail, template)
+
+  mi_plantilla = Plantilla.new(
+      nombre_contacto, 
+      datos_del_mail.nombre_evento, 
+      datos_del_mail.lugar_evento, 
+      datos_del_mail.fecha_del_evento, 
+      datos_del_mail.Mail_de_confirmacion)
+
+  valor_del_template = template
+  valor_a_mostrar = mi_plantilla.armar_plantilla(valor_del_template)
+
+  rhtml = ERB.new(valor_a_mostrar)
+  resultado = rhtml.result(mi_plantilla.get_binding)
+  return resultado
 end
