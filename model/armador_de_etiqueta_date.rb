79c1a4d6b1c261e1e18715c24852484206a87cad
Matias Nievas Soto <matytaa@gmail.com>
2018-06-10 14:47:23 -0300

    implementando soporte etiqueta date

---
+++ model/armador_de_etiqueta_date.rb
@@ -0,0 +1,21 @@
+class ArmadorDeEtiquetaDate
+
+	def armar(plantilla) 
+		expresion = /[<]+([date]+[:]+[d|i])+[>]/
+		una_etiqueta = plantilla.match(expresion)
+		if una_etiqueta != nil
+			un_formato = formato_hora(una_etiqueta.to_s)
+			plantilla.gsub!(expresion) {|etiqueta| etiqueta.gsub(expresion, un_formato)}
+		end
+	end
+
+	def formato_hora(una_etiqueta)
+		formato_de_fecha = nil
+		if una_etiqueta[una_etiqueta.length - 2, 1] == 'i'
+			formato_de_fecha = "%Y-%m-%d"
+		else
+			formato_de_fecha = "%d-%m-%Y"
+		end
+		fecha_actual = Time.now.strftime(formato_de_fecha)
+	end
+end
