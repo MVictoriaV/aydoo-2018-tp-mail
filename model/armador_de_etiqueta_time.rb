78cf8c96090a2c723695a7e45025404547f97c28
Matias Nievas Soto <matytaa@gmail.com>
2018-06-10 15:03:56 -0300

    implementando soporte etiqueta time

---
+++ model/armador_de_etiqueta_time.rb
@@ -0,0 +1,21 @@
+class ArmadorDeEtiquetaTime
+
+	def armar(plantilla) 
+		expresion = /[<]+([time]+[:]+24|[time])+[>]/
+		una_etiqueta = plantilla.match(expresion)
+		if una_etiqueta != nil
+			un_formato = formato_hora(una_etiqueta.to_s)
+			plantilla.gsub!(expresion) {|etiqueta| etiqueta.gsub(expresion, un_formato)}
+		end
+	end
+
+	def formato_hora(una_etiqueta)
+		una_hora = nil
+		if una_etiqueta[una_etiqueta.length - 3, 2] == '24'
+			formato_de_fecha = "%H:%M"
+			una_hora = Time.now.strftime(formato_de_fecha)
+		else
+			una_hora = Time.now.strftime("%R")
+		end
+	end
+end
---
+++ spec/armador_de_etiqueta_time_spec.rb
@@ -0,0 +1,36 @@
+require 'rspec'
+require_relative '../model/armador_de_etiqueta_time'
+
+class ArmadorDeEtiquetaTimeSpec
+
+    describe 'ArmadorDeEtiquetaTime' do
+
+        it 'deberia obtener la fecha en el formato 24' do
+        	formato_de_fecha = "%H:%M"
+          fecha_actual = Time.now.strftime(formato_de_fecha)
+          template = %{<time:24>}
+          un_armador = ArmadorDeEtiquetaTime.new()
+          
+          resultado = un_armador.armar(template)
+          resultado_esperado = fecha_actual.to_s
+          expect(resultado).to include(resultado_esperado)
+        end
+
+        it 'deberia obtener la fecha en el formato DD-MM-YYYY' do
+          fecha_actual = Time.now.strftime("%R")
+          template = %{<time>}
+          un_armador = ArmadorDeEtiquetaTime.new()
+          
+          resultado = un_armador.armar(template)
+          resultado_esperado = fecha_actual.to_s
+          expect(resultado).to include(resultado_esperado)
+        end
+
+        it 'deberia ser nil etiqueta no soportada' do
+          template = %{<time:r>}
+          un_armador = ArmadorDeEtiquetaTime.new()
+          resultado = un_armador.armar(template)
+          expect(resultado).to eq nil
+        end
+    end
+end
