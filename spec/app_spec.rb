ea871ee691674849e71f20a22c432c3fb7383cfc
Victoria <MVictoriaV>
2018-06-10 21:10:02 -0300

    Se elimina test innecesario

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -16,14 +16,6 @@ class AppSpec
 
     parametro_json = File.read("./archivos_de_prueba/datos_prueba1.json")
 
-    describe '/procesadorDeMail/un_nombre' do
-      it 'deberia mostrar por pantalla el mensaje => Hola Juan <=.' do
-        get '/procesadorDeMail/Juan'
-
-        expect(last_response).to be_ok
-      end
-    end
-
     describe '/envio_mail_test' do
       it 'deberia llegar el json y retornar estado ok' do
         post '/envio_mail_test', parametro_json, "CONTENT_TYPE" => "application/json"
