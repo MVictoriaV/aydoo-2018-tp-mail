705135284cd66714c7b770d7fa543cea2606d6bb
Victoria <MVictoriaV>
2018-06-24 11:52:14 -0300

    Email prepara el cuerpo con su respectivo contacto

--- model/email.rb
+++ model/email.rb
@@ -1,13 +1,14 @@
 require_relative 'parseador_contacto'
 require_relative 'punto_entrada_nulo_exception'
+require_relative 'asunto_inexistente_exception'
 require_relative 'remitente_exception'
+require_relative 'armador_de_template'
 
 class EMail
 
+	attr_accessor :contacto_cuerpo_mail
 	attr_accessor :remitente
-	attr_accessor :contactos
 	attr_accessor :asunto
-	attr_accessor :cuerpo
 
 	def initialize(estructura)
 		raise PuntoEntradaNuloException if estructura.nil?
@@ -15,14 +16,37 @@ class EMail
 		verificar_datos_requeridos(estructura)
 		
 		@remitente = estructura[:datos]["remitente"]
+		@asunto = estructura[:datos]["asunto"]
+
 		parseador = ParseadorContacto.new
 		@contactos = parseador.parsear(estructura[:contactos])
-		@asunto = estructura[:datos]["asunto"]
-		@cuerpo = "cuerpo"
+		@valores_para_template = estructura[:datos]
+		@cuerpo = estructura[:template]
+	end
+
+	#Por cada contacto se carga un cuerpo del mail
+	def cargar_cuerpo_a_contacto
+		@contacto_cuerpo_mail = Hash.new
+		armador_template = ArmadorDeTemplate.new
+
+		armador_template.armar(@cuerpo)
+		@contactos.each do |item|
+			plantilla_completa = preparar_datos_para_template(item.get_nombre, armador_template)
+			@contacto_cuerpo_mail[item.get_mail] = plantilla_completa
+		end
 	end
 
 	private
+	attr_accessor :valores_para_template, :contactos, :cuerpo
+
 	def verificar_datos_requeridos(estructura)
 		raise RemitenteException if estructura[:datos]["remitente"].nil?
+		raise AsuntoInexistenteException if estructura[:datos]["asunto"].nil?
+	end
+
+	def preparar_datos_para_template(nombre_contacto, armador_template)
+		@valores_para_template["nombre"] = nombre_contacto
+		plantilla_completa = armador_template.cargar_valores(@valores_para_template)
+		return plantilla_completa
 	end
 end
--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -3,32 +3,56 @@ require 'json'
 require_relative '../model/email'
 require_relative '../model/punto_entrada_nulo_exception'
 require_relative '../model/remitente_exception'
+require_relative '../model/asunto_inexistente_exception'
 
 describe 'EMail' do
 
-  punto_de_entrada = File.read("./archivos_de_prueba/datos_prueba1.json")
-
-  it 'Se crea email con los datos necesarios' do
-    
-    datos_grales = OpenStruct.new(JSON.parse(punto_de_entrada))
-
-    email = EMail.new(datos_grales)
-    
-    expect(email.remitente).to be
-    expect(email.contactos).to be
-    expect(email.asunto).to be
-    expect(email.cuerpo).to be
+  before(:each) do
+      punto_de_entrada = File.read("./archivos_de_prueba/datos_prueba1.json")
+      datos = OpenStruct.new(JSON.parse(punto_de_entrada))
+      @email = EMail.new(datos)
   end
 
-  it 'Se crea email con argumento nil deberia lanzar excepcion' do
+  it 'Se crea email con los datos necesarios' do
+    expect(@email.remitente).to be
+    expect(@email.asunto).to be
+  end
+
+  it 'se crea email con argumento nil deberia lanzar excepcion' do
     expect{EMail.new(nil)}.to raise_exception(PuntoEntradaNuloException)
   end
 
-  it 'No se pasa remitente deberia lanzar excepcion' do
+  it 'no se pasa remitente deberia lanzar excepcion' do
 
     dato_sin_remitente = File.read("./archivos_de_prueba/datos_sin_remitente.json")
     datos_grales = OpenStruct.new(JSON.parse(dato_sin_remitente))
 
     expect{EMail.new(datos_grales)}.to raise_exception(RemitenteException)
+  end
+
+  it 'no se pasa asunto deberia lanzar excepcion' do
+
+    dato_sin_asunto = File.read("./archivos_de_prueba/datos_sin_asunto.json")
+    datos_grales = OpenStruct.new(JSON.parse(dato_sin_asunto))
+
+    expect{EMail.new(datos_grales)}.to raise_exception(AsuntoInexistenteException)
+  end
+
+  it 'deberia cargar 2 emails' do
+
+    @email.cargar_cuerpo_a_contacto
+
+    expect(@email.contacto_cuerpo_mail.key?("juanperez@test.com")).to be_truthy
+    expect(@email.contacto_cuerpo_mail.key?("mariagonzalez@test.com")).to be_truthy
+  end
+
+  it 'deberia tener el cuerpo de email armado correctamente' do
+
+    @email.cargar_cuerpo_a_contacto
+
+    cuerpo_resultado = @email.contacto_cuerpo_mail["juanperez@test.com"]
+    cuerpo_esperado = "Hola juan,\n\r Por medio del presente mail te estamos invitando a la cena de fin de año de la UNTREF, que se desarrollará en el Centro de estudios (avenida Directorio 887, Caseros), el día 5 de diciembre. Por favor confirmar su participación enviando un mail a fiesta@untref.com.\n\rSin otro particular.La direccion"
+
+    expect(cuerpo_resultado).to include(cuerpo_esperado)
   end
 end
