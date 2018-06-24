5d670231d8665a20b7074fa329d537ed525d0d6a
Victoria <MVictoriaV>
2018-06-24 12:17:03 -0300

    Implementacion del armador de mail

--- model/manejador_de_email.rb
+++ model/manejador_de_email.rb
@@ -6,8 +6,16 @@ class ManejadorDeEMail
 
   def armar_email(punto_de_entrada)
     procesador = ProcesadorEstructural.new
-    procesador.crear_estructura(JSON.parse(punto_de_entrada))
+    begin
+      procesador.crear_estructura(JSON.parse(punto_de_entrada))
+    rescue Exception => e
+      return nil
+    end
     mail = EMail.new(procesador.estructura)
     return mail
+  end
+
+  def enviar(email)
+    return !email.nil?
   end
 end
--- spec/manejador_de_email_spec.rb
+++ spec/manejador_de_email_spec.rb
@@ -3,16 +3,37 @@ require_relative '../model/manejador_de_email'
 
 describe 'ManejadorDeEMail' do
 
-	punto_de_entrada = File.read("./archivos_de_prueba/datos_prueba1.json")
-	manejador_de_mail = ManejadorDeEMail.new
+  before(:each) do
+    @manejador_de_mail = ManejadorDeEMail.new
+    @email = double
 
-    it 'Recibe json comienza a armar un email con remitente y asunto' do
-    	un_email = manejador_de_mail.armar_email(punto_de_entrada)
-   		
-        remitente_esperado = 'universidad@untref.com'
-   		asunto_esperado = 'Invitación a fiesta de fin de año'
+    cuerpo = "Hola juan,\n\r Por medio del presente mail te estamos invitando a la cena de fin de año de la UNTREF, que se desarrollará en el Centro de estudios (avenida Directorio 887, Caseros), el día 5 de diciembre. Por favor confirmar su participación enviando un mail a fiesta@untref.com.\n\rSin otro particular.La direccion"
 
-   		expect(un_email.remitente).to eq remitente_esperado
-   		expect(un_email.asunto).to eq asunto_esperado
+    allow(@email).to receive(:contacto_cuerpo_mail).and_return({"juanperez@test.com" => cuerpo})
+    allow(@email).to receive(:remitente).and_return("universidad@untref.com")
+    allow(@email).to receive(:asunto).and_return("Invitación a fiesta de fin de año")
+  end
+
+  it 'recibe json comienza a armar un email con remitente y asunto' do
+    punto_de_entrada = File.read("./archivos_de_prueba/datos_prueba1.json")
+  	un_email = @manejador_de_mail.armar_email(punto_de_entrada)
+ 		
+    remitente_esperado = "universidad@untref.com"
+ 		asunto_esperado = "Invitación a fiesta de fin de año"
+
+ 		expect(un_email.remitente).to eq remitente_esperado
+ 		expect(un_email.asunto).to eq asunto_esperado
+  end
+
+  it 'deberia retornar nil cuando recibe json incorrecto' do
+    punto_de_entrada = File.read("./archivos_de_prueba/data2_esquema_incorrecto.json")
+    respuesta = @manejador_de_mail.armar_email(punto_de_entrada)
+    
+    expect(respuesta).to be_falsey
+  end
+
+  it 'recibe un email que no es nil' do
+    	resultado = @manejador_de_mail.enviar(@email)
+      expect(resultado).to be_truthy
     end
 end
