c169a912d65e62462f4effb3cd082dc761705b9f
Matias Nievas Soto <matytaa@gmail.com>
2018-06-23 21:07:25 -0300

    se agrega excepción de etiqueta time no soportada

--- model/armador_de_etiqueta_time.rb
+++ model/armador_de_etiqueta_time.rb
@@ -1,6 +1,13 @@
+require_relative 'etiqueta_time_no_soportada_exception'
+
 class ArmadorDeEtiquetaTime
 
 	def armar(plantilla) 
+		begin
+			comprobar_etiqueta(plantilla)
+		rescue Exception => msg
+	        puts msg.message
+	    end
 		expresion = /[<]+([time]+[:]+12|[time])+[>]/
 		una_etiqueta = plantilla.match(expresion)
 		if una_etiqueta != nil
@@ -18,5 +25,13 @@ class ArmadorDeEtiquetaTime
 			formato_de_fecha = "%H:%M"
 			una_hora = Time.now.strftime(formato_de_fecha)
 		end
+	end
+
+	def comprobar_etiqueta(plantilla)
+		expresion = /<time+:(?!12).+[>]/
+		una_etiqueta = plantilla.match(expresion)
+		if una_etiqueta != nil
+      		raise EtiquetaTimeNoSoportadaException.new
+    	end
 	end
 end
--- spec/armador_de_etiqueta_time_spec.rb
+++ spec/armador_de_etiqueta_time_spec.rb
@@ -26,14 +26,20 @@ class ArmadorDeEtiquetaTimeSpec
             expect(resultado).to include(resultado_esperado)
         end
 
-        it 'deberia retornar el template original por etiqueta no soportada' do
+        it 'deberia lanzar excepcion de etiqueta time no soportada' do
             template = %{<time:r>}
             un_armador = ArmadorDeEtiquetaTime.new()
-            resultado = un_armador.armar(template)
-            expect(resultado).to include(template)
+            expect{un_armador.comprobar_etiqueta(template)}.to raise_exception(Exception)
         end
 
-        it 'asd' do
+        it 'deberia lanzar excepcion de etiqueta time no soportada' do
+            template = %{<time:24> y <time> }
+            fecha_actual = Time.now.strftime("%R")
+            un_armador = ArmadorDeEtiquetaTime.new()
+            expect{un_armador.comprobar_etiqueta(template)}.to raise_exception(EtiquetaTimeNoSoportadaException)
+        end
+
+        it 'deberia lanzar excepcion de etiqueta time no soportada' do
             template = %{<time:24> y <time> }
             fecha_actual = Time.now.strftime("%R")
             un_armador = ArmadorDeEtiquetaTime.new()
