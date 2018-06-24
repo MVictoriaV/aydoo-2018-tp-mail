164d6bf83c8682d34653a8d302e37744531560bf
Matias Nievas Soto <matytaa@gmail.com>
2018-06-24 00:52:46 -0300

    cambio en mensaje de excepción etiqueta time

--- model/etiqueta_time_no_soportada_exception.rb
+++ model/etiqueta_time_no_soportada_exception.rb
@@ -1,6 +1,6 @@
 class EtiquetaTimeNoSoportadaException < Exception
 
-    def initialize(msg='Existe una etiqueta time no soportada, no se realiza reemplazo sobre la misma.')
+    def initialize(msg='Etiqueta time sin comportamiento definido, no se realiza reemplazo sobre la misma.')
         super
     end
 
