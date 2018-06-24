608e91e50599e722c03ec514279f49aebda45c41
Matias Nievas Soto <matytaa@gmail.com>
2018-06-24 15:06:26 -0300

    se agrega test status 503

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -13,12 +13,27 @@ describe 'Aplicacion Sinatra' do
   end
 
   parametro_json = File.read("./archivos_de_prueba/datos_prueba1.json")
+  parametro_json_incompleto = File.read("./archivos_de_prueba/data2_esquema_incorrecto.json")
 
   describe '/' do
     it 'levanta archivo json con datos incompletos deberia dar un estado 500' do
-        parametro_json = File.read("./archivos_de_prueba/data2_esquema_incorrecto.json")
-        post '/', parametro_json, "CONTENT_TYPE" => "application/json"
+        
+        post '/', parametro_json_incompleto, "CONTENT_TYPE" => "application/json"
         expect(last_response.status).to be == 500
+    end
+  end
+
+  describe '/nuevo' do
+    it 'a levanta archivo json con datos incompletos deberia dar un estado 500' do
+        post '/nuevo', parametro_json_incompleto, "CONTENT_TYPE" => "application/json"
+        expect(last_response.status).to be == 500
+    end
+  end
+
+  describe '/nuevo' do
+    it 'deberia retornar estado 503 servicio SMTP no disponible' do
+        post '/nuevo', parametro_json, "CONTENT_TYPE" => "application/json"
+        expect(last_response.status).to be == 503
     end
   end
 end
