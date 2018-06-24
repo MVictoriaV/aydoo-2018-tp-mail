1774694c2b5a1e2a2aad43fd522c8d71efbc0967
Victoria <MVictoriaV>
2018-06-24 15:16:03 -0300

    Eliminacion de clases deprecadas

--- constantes/constantes_datos.rb
+++ constantes/constantes_datos.rb
@@ -1,29 +1,3 @@
 module Constantes
-
 	ETIQUETAS_REQUERIDAS = ["template", "contactos", "datos"].freeze
-
-	def constantes_requeridas
-		constantes_requeridas = [
-				"template",
-                "contactos",
-                "datos"]
-	end
-
-	def get_constantes_datos
-		constantes_datos = [
-				"remitente", 
-				"asunto", 
-				"nombre_evento", 
-				"lugar_evento", 
-				"fecha_del_evento", 
-				"mail_de_confirmacion"]
-	end
-
-	def get_constantes_contactos
-		constantes_contactos = [
-				"nombre", 
-				"apellido",
-				"mail"
-		]
-	end
 end
