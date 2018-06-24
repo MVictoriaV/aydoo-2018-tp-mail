af22e33a39bb864d8170ff4c986a980d2f39fe78
Matias Nievas Soto <matytaa@gmail.com>
2018-06-24 15:49:08 -0300

    correción test mock envío de mail

--- spec/manejador_de_email_spec.rb
+++ spec/manejador_de_email_spec.rb
@@ -7,32 +7,36 @@ describe 'ManejadorDeEMail' do
     @manejador_de_email = ManejadorDeEMail.new
     @mock_manejador_de_email = double
     @un_archivo_de_contactos_datos_template = File.read("./archivos_de_prueba/datos_prueba1.json")
+    @archivo_esquema_incompleto = File.read("./archivos_de_prueba/data2_esquema_incorrecto.json")
 
     allow(@mock_manejador_de_email).to receive(:armar_email).with(@un_archivo_de_contactos_datos_template).and_return(true)
     allow(@mock_manejador_de_email).to receive(:enviar).and_return(true)
   end
 
+  it 'deberia haber cargado el email correctamente' do
+    resultado = @manejador_de_email.armar_email(@un_archivo_de_contactos_datos_template)
+    expect(resultado).to be_falsey
+  end
+
   it 'recibe json comienza a armar un email con remitente y asunto' do
     punto_de_entrada = File.read("./archivos_de_prueba/datos_prueba1.json")
-  	@manejador_de_email.armar_email(punto_de_entrada)
- 		
-    remitente_esperado = "universidad@untref.com"
- 		asunto_esperado = "Invitación a fiesta de fin de año"
-
- 		expect(@manejador_de_email.email.remitente).to eq remitente_esperado
- 		expect(@manejador_de_email.email.asunto).to eq asunto_esperado
-  end
-
-  it 'deberia retornar nil cuando recibe json incorrecto' do
-    punto_de_entrada = File.read("./archivos_de_prueba/data2_esquema_incorrecto.json")
+    @manejador_de_email.armar_email(punto_de_entrada)
     
-    expect{@manejador_de_email.armar_email(punto_de_entrada)}.to raise_exception(EtiquetaInexistenteException)
+    remitente_esperado = "universidad@untref.com"
+    asunto_esperado = "Invitación a fiesta de fin de año"
+
+    expect(@manejador_de_email.email.remitente).to eq remitente_esperado
+    expect(@manejador_de_email.email.asunto).to eq asunto_esperado
   end
 
-  it 'deberia poder enviar un mail ya que cumple con la estructura necesaria el json recibido' do
-    if @manejador_de_email.armar_email(@un_archivo_de_contactos_datos_template)
-      resultado = @mock_manejador_de_email.enviar
-      expect(resultado).to be_truthy
-    end
+  it 'deberia lanzar excepcion cuando recibe json incompleto' do
+    expect{@manejador_de_email.armar_email(@archivo_esquema_incompleto)}.to raise_exception(EtiquetaInexistenteException)
   end
+
+  it 'deberia poder enviar un mail ya que el atributo email no es nil' do
+    expect(@manejador_de_email.armar_email(@un_archivo_de_contactos_datos_template)).to be_falsey
+    resultado = @mock_manejador_de_email.enviar
+    expect(resultado).to be_truthy
+  end
+
 end
