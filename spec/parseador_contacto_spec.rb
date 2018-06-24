d59a432ed95fa07de686f3bf221874c0b685b321
Matias Nievas Soto <matytaa@gmail.com>
2018-06-24 17:38:05 -0300

    agregando test de validacion de contactos importados

--- spec/parseador_contacto_spec.rb
+++ spec/parseador_contacto_spec.rb
@@ -3,9 +3,7 @@ require_relative '../model/parseador_contacto'
 describe 'ParseadorContacto' do
 
     it 'deberia poder generar un contacto' do
-
         parseador = ParseadorContacto.new
-
         un_contacto_de_entrada = [
             {"nombre"=>"maria", "apellido"=>"gonzalez", "mail"=>"mariagonzalez@test.com"}]
 
@@ -15,9 +13,7 @@ describe 'ParseadorContacto' do
     end
 
     it 'deberia tener un array de 2 contactos' do
-        
         parseador = ParseadorContacto.new
-
         dos_contactos_de_entrada = [
             {"nombre"=>"juan", "apellido"=>"perez", "mail"=>"juanperez@test.com"},
             {"nombre"=>"maria", "apellido"=>"gonzalez", "mail"=>"mariagonzalez@test.com"}]
@@ -28,13 +24,43 @@ describe 'ParseadorContacto' do
     end
 
     it 'deberia ser cero la cantidad de contactos retornada' do
-        
         parseador = ParseadorContacto.new
-
         sin_contactos = []
-
         parseador.parsear(sin_contactos)
-
         expect(parseador.contactos.size).to eq 0
-    end    
+    end
+
+    it 'deberia tener un array de 1 contacto ya que falta el nombre de uno de los dos contactos' do
+        parseador = ParseadorContacto.new
+        dos_contactos_de_entrada = [
+            {"nombre"=>"kimi", "apellido"=>"raikkonen", "mail"=>"kimi.raikkonen@unmail.com"},
+            {"nombre"=>"", "apellido"=>"ricciardo", "mail"=>"daniel.ricciardo@unmail.com"}]
+
+        parseador.parsear(dos_contactos_de_entrada)
+
+        expect(parseador.contactos.size).to eq 1
+    end
+
+    it 'deberia tener un array de 1 contacto ya que falta el apellido de uno de los dos contactos' do
+        parseador = ParseadorContacto.new
+        dos_contactos_de_entrada = [
+            {"nombre"=>"kimi", "apellido"=>"raikkonen", "mail"=>"kimi.raikkonen@unmail.com"},
+            {"nombre"=>"daniel", "apellido"=>"", "mail"=>"daniel.ricciardo@unmail.com"}]
+
+        parseador.parsear(dos_contactos_de_entrada)
+
+        expect(parseador.contactos.size).to eq 1
+    end
+
+    it 'deberia tener un array de 1 contacto ya que falta el mail de uno de los dos contactos' do
+        parseador = ParseadorContacto.new
+        dos_contactos_de_entrada = [
+            {"nombre"=>"kimi", "apellido"=>"raikkonen", "mail"=>"kimi.raikkonen@unmail.com"},
+            {"nombre"=>"daniel", "apellido"=>"ricciardo", "mail"=>""}]
+
+        parseador.parsear(dos_contactos_de_entrada)
+
+        expect(parseador.contactos.size).to eq 1
+    end
+
 end
